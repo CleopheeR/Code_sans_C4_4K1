2dac196dce3e4d8755f7090d6c56e19dcc8e765e
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2021-10-16 17:51:16 +0200

    maintenant recherche des fixeurs parallélisée

--- fixage.cpp
+++ fixage.cpp
@@ -12,17 +12,18 @@
 
 
 
-vector<char> bigDegreeList;
+vector<char> bigDegreeList[NBMAXPROC];
+Graph isPreOrFixeurGWithEdges[NBMAXPROC];
 
-bool is_pre_or_fixeur(Graph &g, vector<char> &degreeList, bool prefixeurTest, sparse_hash_map<vector<char>, vector<Graph>> &prefixeurPlusDict, int **isTwinCompat)
+bool is_pre_or_fixeur(const Graph &g, bool prefixeurTest, const sparse_hash_map<vector<char>, vector<Graph>> &prefixeurPlusDict, int **isTwinCompat, int idThread)
 {
     int nbVert = g.nbVert+1;
     const int puissNewVert = (1<<(nbVert-1));
     const int nbEdgeCombi = (1<<(nbVert-1));
 
-    if (bigDegreeList.empty())
-        bigDegreeList.resize(g.nbVert+5);
-    static Graph gWithEdges;
+    if (bigDegreeList[idThread].empty())
+        bigDegreeList[idThread].resize(g.nbVert+5);
+    Graph &gWithEdges = isPreOrFixeurGWithEdges[idThread];
     if (gWithEdges.adjMat == NULL)
         gWithEdges.init(g.nbVert+1, -1);
     bool printDebug = false;
@@ -39,7 +40,8 @@ bool is_pre_or_fixeur(Graph &g, vector<char> &degreeList, bool prefixeurTest, sp
     //twinLists2.clear();
     //gen_twin_list(g, twinLists2, nbVert);
 
-
+    if (g.nbVert == 0)
+        exit(78);
     gen_P2_list(g, pathLength2, nbVert);
 
 
@@ -88,14 +90,20 @@ bool is_pre_or_fixeur(Graph &g, vector<char> &degreeList, bool prefixeurTest, sp
             return false;
 
         bool isGBigPreFixeur = false;
+        vector<char> &curBigDegreeList = bigDegreeList[idThread];
 
         for (int i = 0; i < gWithEdges.nbVert; i++)
-            bigDegreeList[i] = gWithEdges.get_neighb(i).size();
-        sort(bigDegreeList.begin(), bigDegreeList.begin()+gWithEdges.nbVert);
+            curBigDegreeList[i] = gWithEdges.get_neighb(i).size();
+        sort(curBigDegreeList.begin(), curBigDegreeList.begin()+gWithEdges.nbVert);
         //gWithEdges.compute_hashes(bigDegreeList);
-        for (const Graph& gSeen : prefixeurPlusDict[bigDegreeList])
+        const auto &itPrefixeursPlus1ToTest = prefixeurPlusDict.find(curBigDegreeList);
+        //for (const Graph& gSeen : prefixeurPlusDict[bigDegreeList])
+        if (itPrefixeursPlus1ToTest == prefixeurPlusDict.cend())
+            return false;
+        const vector<Graph> &prefixeursPlus1ToTest = itPrefixeursPlus1ToTest->second;
+        for (const Graph& gSeen : prefixeursPlus1ToTest)
         {
-            if (are_isomorphic(gWithEdges, gSeen))
+            if (are_isomorphic(gWithEdges, gSeen, idThread))
             {
                 isGBigPreFixeur = true;
                 break;
@@ -114,18 +122,27 @@ bool is_pre_or_fixeur(Graph &g, vector<char> &degreeList, bool prefixeurTest, sp
 
 sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
 {
-    sparse_hash_map<vector<char>, vector<Graph>> deglist2Fixeurs;
-
+     sparse_hash_map<vector<char>, vector<Graph>> deglist2Fixeurs;
     sparse_hash_map<vector<char>, vector<Graph>> deglist2PrefixeursPlus;
-
     vector<char> degreeList(nbVert+4);
     vector<char> degreeListPlus(nbVert+5);
+
+
     int nbEdgeCombi = 1<<nbVert;
 
-    stringstream fileName;
-    fileName << "Alexgraphedelataille";
-    fileName << nbVert << ".txt";
-    vector<Graph> listGraphs = load_from_file(fileName.str());
+    stringstream fileName, fileSizeName;
+    fileName << "Alexgraphedelataille" << nbVert << ".txt.gz";
+    fileSizeName << "Alexsizegraphedelataille" << nbVert << ".txt";
+    ifstream fSize(fileSizeName.str());
+    if (fSize.peek() == EOF)
+    {
+        cerr << "Lancer avant la taille -1 size \n";
+        cerr << fileSizeName.str() << endl;
+        exit(3);
+    }
+    int nbGToRead;
+    fSize >> nbGToRead;
+    vector<Graph> listGraphs = load_from_file(fileName.str(), nbGToRead);
     if (listGraphs.empty())
     {
         cerr << "Erreur : lancer avant la génération de la même taille \n";
@@ -136,7 +153,7 @@ sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
     fileNamePlus << "Alexfixeursdelataille";
     fileNamePlus << nbVert+1 << ".txt";
 
-    igzstream filePlus(fileNamePlus.str());
+    igzstream filePlus(fileNamePlus.str().c_str());
     int nbPlus;
     if (filePlus.peek() != EOF)
     {
@@ -160,9 +177,8 @@ sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
     cout << "j'ai généré/trouvé les graphes à " << nbVert << " somets : il y en a " << listGraphs.size() << endl;
 
 
-
     int **isTwinCompat = NULL;
-    /*
+
     isTwinCompat = (int**) malloc(sizeof(*isTwinCompat)*nbEdgeCombi);
     for (int i = 0; i < nbEdgeCombi; i++)
         isTwinCompat[i] = (int*) malloc(sizeof(*isTwinCompat)*NBMAXVERT);
@@ -187,7 +203,43 @@ sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
             }
             isTwinCompat[code][v1] = curCompat;
         }
-    }*/
+    }
+
+
+
+    // Nouvelle version avec la parallélisation :-)
+    vector<Graph> fixeursList;
+    fixeursList.reserve(1000000);
+
+    int nbPerProc = listGraphs.size()/nbProc;
+    mutex threadMutex;
+    vector<thread> threads(nbProc-1);
+    for (int iProc = 0; iProc < nbProc-1; iProc++)
+        threads[iProc] = thread(&gen_fixeurs_thread, nbVert, std::cref(listGraphs), isTwinCompat, std::ref(fixeursList), std::cref(deglist2PrefixeursPlus), iProc*nbPerProc, (iProc+1)*nbPerProc, std::ref(threadMutex), iProc);
+
+    thread lastProc = thread(&gen_fixeurs_thread, nbVert, std::cref(listGraphs), isTwinCompat, std::ref(fixeursList), std::cref(deglist2PrefixeursPlus), (nbProc-1)*nbPerProc, listGraphs.size(), std::ref(threadMutex), nbProc-1);
+
+    lastProc.join();
+    for (int iProc = 0; iProc < nbProc-1; iProc++)
+        threads[iProc].join();
+
+    cout  << "Il y a " << fixeursList.size() << " fixeurs à " << nbVert << " sommets.\n";
+    stringstream fixeursFileNamee;
+    fixeursFileNamee << "Alexfixeursdelataille";
+    fixeursFileNamee << nbVert << ".txt";
+
+    //save_to_file(fixeursFileName.str(), deglist2Fixeurs, nbGraph);
+    //TODO maybe refaire fonction ?
+    ogzstream outFile(fixeursFileNamee.str().c_str());
+    outFile << fixeursList.size() << endl;
+    for (const Graph &g : fixeursList)
+        g.print_in_file(outFile);
+    outFile.close();
+
+
+
+
+    return deglist2Fixeurs;
 
 
 
@@ -198,7 +250,7 @@ sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
         if (cptGraph%1000 == 0)
             cerr << "Nous sommes sur le " << cptGraph << "-ème graphe sur " << listGraphs.size() << endl;
 
-        if (is_pre_or_fixeur(g, degreeList, true, deglist2PrefixeursPlus, isTwinCompat))
+        if (is_pre_or_fixeur(g, true, deglist2PrefixeursPlus, isTwinCompat, 0))
         {
             for (int i = 0; i < g.nbVert; i++)
                 degreeList[i] = g.get_neighb(i).size();
@@ -221,3 +273,31 @@ sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert)
 
     return deglist2Fixeurs;
 }
+
+
+void gen_fixeurs_thread(int nbVert, const vector<Graph> &graphList, int** isTwinCompat, vector<Graph> &fixeursList, const sparse_hash_map<vector<char>, vector<Graph>> &deglist2PrefixeursPlus, int iBeg, int iEnd, mutex &mutInsert, int idThread)
+{
+    vector<char> degreeListPlus(nbVert+5);
+    int nbEdgeCombi = 1<<nbVert;
+
+
+    int cptGraph = 0;
+
+    for (int iG = iBeg; iG < iEnd; iG++)
+    {
+        //cerr << " coucou " << iG << " (" << idThread << endl;
+        const Graph &g = graphList[iG];
+        cptGraph++;
+        if (cptGraph%10000 == 0)
+            cerr << "Nous sommes sur le " << cptGraph << "-ème graphe sur " << iEnd-iBeg << "(" << iBeg << "," << iEnd << ")" << endl;
+
+        if (is_pre_or_fixeur(g, true, deglist2PrefixeursPlus, isTwinCompat, idThread))
+        {
+            cerr << "YYYYYYYYYYYYYYYYEEEEEEEEESSSSSSSSSS\n";
+            mutInsert.lock();
+            fixeursList.push_back(g);
+            mutInsert.unlock();
+        }
+    }
+}
+
--- fixage.hh
+++ fixage.hh
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <map>
+#include <mutex>
+
 #include "Graph.hh"
 #include "sparsepp/spp.h"
 
@@ -10,8 +12,10 @@ using spp::sparse_hash_map;
 
 bool is_fixeur(const Graph& g, const vector<vector<int>> &listSubsetsEdges);
 
-bool is_pre_or_fixeur(Graph &g, vector<char> &degreeList, bool prefixeurTest, sparse_hash_map<vector<char>, vector<Graph>> &prefixeurPlusDict, int **isTwinCompat);
+bool is_pre_or_fixeur(const Graph &g, bool prefixeurTest, const sparse_hash_map<vector<char>, vector<Graph>> &prefixeurPlusDict, int **isTwinCompat, int idThread);
 
 
-sparse_hash_map<vector<int>, vector<Graph>> gen_fixeurs(int nbVert);
+sparse_hash_map<vector<char>, vector<Graph>> gen_fixeurs(int nbVert);
+
+void gen_fixeurs_thread(int nbVert, const vector<Graph> &graphList, int** isTwinCompat, vector<Graph> &fixeursList, const sparse_hash_map<vector<char>, vector<Graph>> &deglist2PrefixeursPlus, int iBeg, int iEnd, mutex &mutInsert, int idThread);
 #endif
